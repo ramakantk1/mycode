ac6463c89e42b50dc1428a06c695154ae5fe23ef
‘Ramakant <ramakantk@cybage.com>
2020-07-18 20:31:26 +0530

    added fibonacci program

--- fibonacci/myfibonacci.go
+++ fibonacci/myfibonacci.go
@@ -1,4 +1,4 @@
-//Https://play.golang.org/p/Hh8Owj1CXp4
+//https://play.golang.org/p/4gluMnl-vDj
 
 package main
 
@@ -7,17 +7,31 @@ import (
 )
 
 func main() {
+	/* without recursive*/
+	/*
+		x, y := 0, 1
+		if x == 0 {
+			fmt.Println(0)
+			fmt.Println(1)
+		}
+		for i := 0; i < 10; i++ {
+			fmt.Println(x + y)
+			tempi := x
+			x, y = y, tempi+y
 
-	x, y := 0, 1
-	if x == 0 {
-		fmt.Println(0)
-		fmt.Println(1)
+		}
+	*/
+
+	for i := 0; i <= 9; i++ {
+		fmt.Println(FibonacciRecursion(i))
 	}
-	for i := 0; i < 10; i++ {
-		fmt.Println(x + y)
-		tempi := x
-		x, y = y, tempi+y
 
+}
+
+func FibonacciRecursion(n int) int {
+	if n <= 1 {
+		return n
 	}
 
+	return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
 }
